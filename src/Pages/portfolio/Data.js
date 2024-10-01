51dbb6e38a1a57a52384467576e31fc462e9e25a
saketh612 <saketh.repaka01@gmail.com>
2024-10-02 00:26:42 +0530

    commit all

--- src/Pages/portfolio/Data.js
+++ src/Pages/portfolio/Data.js
@@ -8,22 +8,46 @@ const image = 'https://s3-ap-south-1.amazonaws.com/static.awfis.com/wp-content/u
 export const projectsData = [
     {
         id: 1,
-        image: image,
-        title: "Project 1",
+        image: 'https://img.freepik.com/free-photo/cartoon-style-traveling-concept-with-baggage_23-2151129795.jpg',
+        title: "AI-Travel Planner",
         category: "Frontend",
+        link:'https://github.com/sakethrepaka/TravelApp'
     },
     {
         id: 2,
-        image: image,
-        title: "Project 2",
-        category: "Mobile",
+        image: 'https://static1.makeuseofimages.com/wordpress/wp-content/uploads/2021/12/demonetization-youtube.jpg',
+        title: "Youtube-Clone",
+        category: "Frontend",
+        link:'https://github.com/sakethrepaka/React-Youtube-Clone'
     },
     {
         id: 3,
-        image: image,
-        title: "Project 3",
-        category: "FullStack",
+        image: 'https://i0.wp.com/www.muscleandfitness.com/wp-content/uploads/2018/11/Group-Fitness-Class-Performing-A-Variety-Of-Exercises-1.jpg?quality=86&strip=all',
+        title: "Exercise Applicaion",
+        category: "Frontend",
+        link:'https://github.com/sakethrepaka/exercise_application'
     },
+    {
+        id: 4,
+        image: 'https://content.jdmagicbox.com/comp/suryapet/s1/9999p8684.8684.190313101350.a9s1/catalogue/hi7-food-delivery-suryapet-nvbk6vythv.jpg',
+        title: "Food Delivery App",
+        category: "Mobile",
+        link:'https://github.com/sakethrepaka/foodie'
+    },
+    {
+        id: 5,
+        image: 'https://www.budgetbytes.com/wp-content/uploads/2013/07/How-to-Calculate-Recipe-Costs-H.jpg',
+        title: "Recipe-ChatApp",
+        category: "Mobile",
+        link:'https://github.com/sakethrepaka/recipes'
+    },
+    {
+        id: 6,
+        image: 'https://images.klipfolio.com/website/public/fef9b3dd-f994-40ec-ba1c-c514e551a587/Business%20Dashboard.png',
+        title: "Multi-Purpose Dashboards",
+        category: "FullStack",
+        link:'https://github.com/sakethrepaka/'
+    }
 ];
 
 export const projectsNav = [
--- src/Pages/portfolio/PorftolioItems.js
+++ src/Pages/portfolio/PorftolioItems.js
@@ -6,8 +6,8 @@ const PortfolioItems = ({item}) => {
         <div className="project__card" key={item.id}>
             <img className="project__img" src={item.image} alt="" />
             <h3 className="project__title">{item.title}</h3>
-            <a href="#" className="project__button">
-                Demo <HiOutlineArrowSmRight className="project__button-icon" />
+            <a href={item.link} target='_blank'className="project__button">
+                Source Code <HiOutlineArrowSmRight className="project__button-icon" />
             </a>
         </div>
     );
