635a79c931d35af0aeb6a3f3e3ee7cf87ac1ef46
Saketh <saketh.repaka01@gmail.com>
2026-02-25 18:16:44 +0000

    Updated experience and qualaification data

--- src/Pages/about/Info.js
+++ src/Pages/about/Info.js
@@ -7,7 +7,7 @@ const Info = () => {
         <div className="about__box">
             <HiOutlineDesktopComputer className="about__icon" />
             <h3 className="about__title">Experience</h3>
-            <span className="about__subtitle">1 Year</span>
+            <span className="about__subtitle">2+ Years</span>
         </div>
         <div className="about__box">
             <HiOutlineTerminal className="about__icon" />
@@ -17,7 +17,7 @@ const Info = () => {
         <div className="about__box">
             <HiOutlineSparkles className="about__icon" />
             <h3 className="about__title">Availability</h3>
-            <span className="about__subtitle">Immediate</span>
+            <span className="about__subtitle">One Month Notice Period</span>
         </div>
     </div>
   );
--- src/Pages/qualification/Qualification.js
+++ src/Pages/qualification/Qualification.js
@@ -98,12 +98,27 @@ const Qualification = () => {
                             <span className="qualification__subtitle">Brane</span>
                             <div className="qualification__calendar">
                                 <HiOutlineCalendar className="qualification__calendar-icon" />
-                                Jan 2024- Present
+                                Jan 2024- Sep 2024
                             </div>
                         </div>
                         <div>
                             <span className="qualification__rounder"></span>
                             <span className="qualification__line"></span>
+                        </div>
+                    </div>
+                                        <div className="qualification__data">
+                        <div></div>
+                        <div>
+                            <span className="qualification__rounder"></span>
+                            <span className="qualification__line"></span>
+                        </div>
+                        <div>
+                            <h3 className="qualification__title">SDE</h3>
+                            <span className="qualification__subtitle">Murf AI</span>
+                            <div className="qualification__calendar">
+                                <HiOutlineCalendar className="qualification__calendar-icon" />
+                                Dec 2024 - Present
+                            </div>
                         </div>
                     </div>
                 </div>
